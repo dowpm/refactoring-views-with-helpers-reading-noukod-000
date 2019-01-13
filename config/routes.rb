6aae7a10375d685a4655c5c7e65bd54b64c37e1b
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-13 06:31:57 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # get 'authors/show', to: 'authors#show', as: 'author'
-  resource :authors
+  get 'authors/show', to: 'authors#show', as: 'author'
+  # resource :authors
 
   get 'test/index'
 
